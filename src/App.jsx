2488699ecd4cac14a3f33c9b6424bc89239ed165
thijsBoet <m.boet@chello.nl>
2023-04-06 12:25:33 +0200

    ratelimited for too many request to OPEN_AI server

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
+import { useState } from 'react';
 import { Container, Box } from '@chakra-ui/react';
 import Header from './components/Header';
 import TextInput from './components/TextInput';
 import Footer from './components/Footer';
 
-
 const App = () => {
-  const extractKeywords = (text) => {
-    console.log(text);
-  };
+	const [keywords, setKeywords] = useState('');
+	const [isOpen, setIsOpen] = useState(false);
+	const [loading, setLoading] = useState(false);
+
+	const extractKeywords = async (text) => {
+		setLoading(true);
+		setIsOpen(true);
+
+		const options = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${import.meta.env.VITE_OPEN_AI_API_KEY}`,
+			},
+			body: JSON.stringify({
+				model: 'text-davinci-003',
+				prompt:
+					'Extract keywords from this text. Make the first letter of every word uppercase and separate with commas:\n\n' +
+					text +
+					'',
+				temperature: 0.5,
+				max_tokens: 60,
+				top_p: 1.0,
+				frequency_penalty: 0.8,
+				presence_penalty: 0.0,
+			}),
+		};
+
+		try {
+			const response = await fetch(
+				import.meta.env.VITE_OPEN_AI_API_URL,
+				options,
+			);
+			const json = await response.json();
+			console.log(json.choices[0].text.trim());
+			setKeywords(json.choices.text.trim());
+			setLoading(false);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
 	return (
 		<Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
