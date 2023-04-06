dff1ce651025bb564465e44afb9616f22bbe0bd0
thijsBoet <m.boet@chello.nl>
2023-04-06 11:45:40 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Container, Box } from '@chakra-ui/react';
+import Header from './components/Header';
+import TextInput from './components/TextInput';
+import Footer from './components/Footer';
+
+
+const App = () => {
+  const extractKeywords = (text) => {
+    console.log(text);
+  };
+
+	return (
+		<Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
+			<Container maxW='3xl' centerContent>
+				<Header />
+				<TextInput extractKeywords={extractKeywords} />
+				<Footer />
+			</Container>
+		</Box>
+	);
+};
+
+export default App;
